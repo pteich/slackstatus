99633840b3f35ef6483d656c74191039c637c126
Peter Teich <peter.teich@motor.de>
2016-11-21 19:58:20 +0100

    added cli functionality

---
+++ slackstatus/cmd/root.go
@@ -0,0 +1,107 @@
+// Copyright © 2016 Peter Teich <mail@pteich.xyz>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"log"
+
+	"github.com/pteich/slackstatus"
+)
+
+var cfgFile string
+
+var RootCmd = &cobra.Command{
+	Use:   "slackstatus",
+	Short: "Post a formatted status message to Slack",
+	Long: `Post a formatted status message to a Slack channel with a given
+username. An optional footer info can be provided.
+You can set one of the predifined colors DANGER, WARNING or GOOD or use any hex color value.
+
+You need to set up an incoming webhook for your Slack at https://my.slack.com/services/new/incoming-webhook/ first.
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if len(args) < 1 {
+			log.Fatal("You need to provide a message to post as argument")
+		}
+
+		var slackmsg = slackstatus.Message{
+			WebhookURL: viper.GetString("webhook"),
+			Username: viper.GetString("user"),
+			Channel: viper.GetString("channel"),
+			IconEmoji: viper.GetString("iconemoji"),
+			Footer: viper.GetString("footer"),
+		}
+
+		if err := slackmsg.Send(args[0], viper.GetString("color")); err != nil {
+			log.Fatal(err)
+		}
+
+	},
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.slackstatus.yaml)")
+
+	RootCmd.PersistentFlags().String("webhook", "", "Slack webhook URL (required)")
+	RootCmd.MarkFlagRequired("webhook")
+
+	RootCmd.PersistentFlags().String("user", "slackstatus", "Username to use when posting")
+	RootCmd.PersistentFlags().String("channel", "#slackstatus", "Channel to post into")
+	RootCmd.PersistentFlags().String("iconemoji", ":monkey_face:", "Icon emoji to use whan posting")
+	RootCmd.PersistentFlags().String("footer", "", "Footer text")
+	RootCmd.PersistentFlags().String("color", "good", "Color for message bar")
+
+	viper.BindPFlags(RootCmd.PersistentFlags())
+
+	log.SetFlags(0)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".slackstatus")
+	viper.AddConfigPath(os.Getenv("HOME"))
+	viper.AddConfigPath(".")
+	viper.SetEnvPrefix("slackstatus")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
